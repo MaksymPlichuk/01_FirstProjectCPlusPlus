b0f7dacd804ae8d2a1a793800c4dede3e459276c
mka <mka@A21PC6>
2025-02-24 17:41:12 +0200

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+//void main()
+//{
+//	//Cntrl alt + L yaksho nemay papok
+//	//Cntrl+d - Dublicate
+//	//Cntrl + K + C - Comment
+//	//Cntrl + K + u - UNComment
+// 	cout << "Hello world";
+//	cout << "Hello world\n";
+//	cout << "Atella warld\n";
+//	cout << "Atithello worldin";
+//	cout << "Hello world";
+//	cout << "Wiello world\\\n";
+//	cout << "Hello world/in";
+//	cout << "hello world\'\n";
+//	cout << "Hello world";
+//}
+
+void main()
+{
+	cout << "I tried so hard and got so far But in the end, it doesnt even matter I had to fall to lose it all But in the end, it doesnt even matter\n";
+			 
+	cout << "Every\n \thunter\n \t\twants\n \t\t\tto\n \t\t\t\tknow\n \t\t\t\t\twhere\n \t\t\t\t\t\tthe\n \t\t\t\t\t\t\tpheasant\n \t\t\t\t\t\t\t\t\tis.\n";
+
+	cout << "Selling dogs.\n Type: Roytveller. \n ...\n Phone: 222-22-22\n 222\t | 222 |\n 22\t | 22 |";
+
+}
